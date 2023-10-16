e5b676f55ae275e4f16b10593b69cf6e2e89fbb0
Simphiwek <simphiwe.olwethu@jumpco.io>
2023-10-16 22:42:29 +0200

    Updated using JFRAME

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,75 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
+
 public class Main {
+    public static class  CurrencyConverter extends JFrame implements ActionListener {
+
+        private JTextField amountTextField, resultTextField;
+        private JComboBox<String> currencyComboBox;
+
+        public CurrencyConverter () {
+            //Setting the Jframe
+            setTitle("Currency Converter");
+            setSize(400, 200);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            //Creating Components
+            JLabel amountLabel = new JLabel("Enter amount");
+            amountTextField = new JTextField(10);
+
+            JLabel currencyLabel = new JLabel("Select currency:");
+            String[] currencies = {"USD", "ZAR", "EUR", "GBP", "CAD", "AUD", "JPY", "INR"};
+            currencyComboBox = new JComboBox<>(currencies);
+
+            JButton convertButton = new JButton("Convert");
+            resultTextField = new JTextField(15);
+            resultTextField.setEditable(false);
+
+            //Layout Manager
+            setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+            //Adding all the components into JFrame
+            add(amountLabel);
+            add(amountTextField);
+            add(currencyLabel);
+            add(currencyComboBox);
+            add(convertButton);
+            add(resultTextField);
+
+            //adding a listener to the button
+            convertButton.addActionListener(this);
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getActionCommand().equals("Convert")){
+                double amount = Double.parseDouble(amountTextField.getText());
+                String currency = (String) currencyComboBox.getSelectedItem();
+
+                //Convertion Calculations performed
+                double total;
+                if (currency.equals("USD")){
+                    total = amount * 0.067;
+                    resultTextField.setText("Converted to Dollar: $" + total);
+                } else if (currency.equals("ZAR")) {
+                    total = amount * 18.77;
+                    resultTextField.setText("Converted to Rand: R" + total);
+                }
+
+            }
+        }
+
+
+    }
+
 
     public static void main(String [] args){
-
-        scanner sc = new Scanner(System.in);
-
-        double amount = sc.nextD
-        JFrame frame = new JFrame("Curreny converter");
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        JLabel label = new JLabel("Converter");
-        JButton button = new JButton();
-        button.setText("Convert");
-        panel.add(label);
-        panel.add(button);
-        frame.add(panel);
-        frame.setSize(300, 400);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            CurrencyConverter converter = new CurrencyConverter();
+            converter.setVisible(true);
+        });
     }
 }
